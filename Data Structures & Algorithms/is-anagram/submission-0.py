86eebe70b52cbb64880dae61976a29fdd84f11ec
Amine <amine.elattllati@gmail.com>
2026-09-19 13:54:12 +0100

    Update: is-anagram - submission-0

--- Data Structures & Algorithms/is-anagram/submission-0.py
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -1,16 +1,13 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        
-
-
         if len(s) != len(t):
             return False
+        countS, countT = {}, {}
 
-        CountS, CountT = {}, {}
         for i in range(len(s)):
-            CountT[s[i]] = 1 + CountT.get(s[i], 0)
-            CountS[t[i]] = 1 + CountS.get(t[i], 0)
-        for c in CountT:
-            if CountT[c] != CountS.get(c, 0):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+        for c in countS:
+            if countS[c] != countT.get(c, 0):
                 return False
         return True
